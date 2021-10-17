8fd24a18ac0ed74265bb62c42c4a222fa7b7ff5d
marahmusleh <marahaltarefe18@gmail.com>
2021-10-17 20:54:31 +0300

    first init

---
+++ src/test/java/com/application2/codeFellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application2.codeFellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
